18bd820b55dd999a14ca8044af9bfa1eb1085faf
viettdnde140214 <viettdnde140214@outlook.com>
2023-09-15 22:03:17 +0700

    test commit 2

--- Test_Git/Program.cs
+++ Test_Git/Program.cs
@@ -14,6 +14,7 @@
             Console.WriteLine("Viet update master");
             Console.WriteLine("Viet test merge add-new-3 into master");
             Console.WriteLine("test commit 1");
+            Console.WriteLine("test commit 2");
         }
     }
 }
