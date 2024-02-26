2afe8ee8e3fe39ff475861a661bc062534e877b1
viettdnde140214 <viettdnde140214@outlook.com>
2024-02-26 22:55:19 +0700

    feature/test_8 part 1

--- Test_Git/Program.cs
+++ Test_Git/Program.cs
@@ -43,6 +43,8 @@
             Console.WriteLine("feature/test-6");
             Console.WriteLine("feature/test-6");
             Console.WriteLine("feature/test-6");
+            ///////////////////////////////////
+            Console.WriteLine("feature/test_8");
         }
     }
 }
